56a570b4234f2e3c52e18f8a7a881c6f0e788419
Rua-A <rootxbot@gmail.com>
2021-07-27 04:20:24 +0900

    0726~27_5.0~5.6_makePugfile

--- src/server.js
+++ src/server.js
@@ -1,21 +1,24 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "../routers/globalRouter";
-import videoRouter from "../routers/videoRouter";
-import userRouter from "../routers/userRouter";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
 //아래의 express어플리케이션을 지정하고 코드를 작성해야 한다.
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine","pug");
+app.set("views",process.cwd() + "/src/views");
+app.use(logger);
 app.use("/",globalRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
 
 const handleListening = () =>
-  console.log(`Server listening in port http://localhost:${PORT}!!`);
+  console.log(`Server listening in port http://localhost:${PORT}  !!`);
 app.listen(PORT, handleListening);
 
 
